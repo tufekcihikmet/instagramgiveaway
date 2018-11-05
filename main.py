4c9594db303f848360b62352787a23bdcf780c40
tufekcihikmet <tufekcihikmet@hotmail.com>
2018-11-06 00:07:03 +0300

    added get usernames from post

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 import urllib.request
+import time
 
 class InstagramBot():
     def __init__(self, email, password):
-        self.browser = webdriver.Chrome()
+        chrome_options = Options()
+        #chrome_options.add_argument("--headless")
+        self.browser = webdriver.Chrome(chrome_options=chrome_options)
         self.email = email
         self.password = password
 
@@ -18,11 +22,9 @@ class InstagramBot():
         emailInput.send_keys(self.email)
         passwordInput.send_keys(self.password)
         passwordInput.send_keys(Keys.ENTER)
-        time.sleep(2)
 
     def getImage(self):
         self.browser.get('https://www.instagram.com/explore/tags/uniday18/')
-        time.sleep(2)
         images = self.browser.find_elements_by_tag_name('img')
         for image in images:
             print(image.get_attribute('src'))
@@ -30,7 +32,28 @@ class InstagramBot():
             url = image.get_attribute('src')
             urllib.request.urlretrieve(url, filename)
 
+    def getComments(self):
+        #give post url
+        self.browser.get('https://www.instagram.com/p/BpsdJ56gnfZ/')
+
+        #loads all comments by clicking load more button
+        loadmore_XP = "//button[contains(@class,'Z4IfV _0mzm- sqdOP yWX7d        ')]"
+        loadmorebutton = self.browser.find_element_by_xpath(loadmore_XP)
+        while True :
+            try:
+                loadmorebutton = self.browser.find_element_by_xpath(loadmore_XP)
+                loadmorebutton.click()
+                time.sleep(1)
+            except NoSuchElementException:
+                break
+
+        #gets usernames
+        username_XP = "//a[contains(@class,'FPmhX notranslate TlrDj')]"
+        users = self.browser.find_elements_by_xpath(username_XP)
+        for user in users:
+            print(user.get_attribute('title'))
+
 
 bot = InstagramBot("alikocakacgun", "SelambenHick")
-bot.signIn()
-bot.getImage()
+
+bot.getComments()
